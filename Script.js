a597a0716d58c079c84c7aa772174d675c06da1e
Amit Kumar <amitkumar9110311@gmail.com>
2026-06-03 20:28:01 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,39 @@
+const words = ["Developer.", "Designer.", "Creator."];
+let i = 0;
+let timer;
+
+function typingEffect() {
+    let word = words[i].split("");
+    var loopTyping = function() {
+        if (word.length > 0) {
+            document.getElementById('changing-txt').innerHTML += word.shift();
+        } else {
+            setTimeout(deletingEffect, 2000); // Pause before deleting
+            return false;
+        }
+        timer = setTimeout(loopTyping, 150); // Typing speed
+    };
+    loopTyping();
+}
+
+function deletingEffect() {
+    let word = words[i].split("");
+    var loopDeleting = function() {
+        if (word.length > 0) {
+            word.pop();
+            document.getElementById('changing-txt').innerHTML = word.join("");
+        } else {
+            if (words.length > (i + 1)) {
+                i++;
+            } else {
+                i = 0;
+            }
+            setTimeout(typingEffect, 500); // Pause before next word
+            return false;
+        }
+        timer = setTimeout(loopDeleting, 100); // Deleting speed
+    };
+    loopDeleting();
+}
+
+typingEffect();
